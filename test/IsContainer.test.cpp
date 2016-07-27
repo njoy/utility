3c9b6f12db5b593fb7d0aeba0a56d4da7461af91
Austin McCartney <amccartney@lanl.gov>
2016-07-27 16:07:07 -0600

    Two more SFINAE exercises

--- test/IsContainer.test.cpp
+++ test/IsContainer.test.cpp
@@ -10,10 +10,13 @@ extern int testNumber;
 SCENARIO("IsContainer alias works correctly",
 	 "[utility]"){
   bool test;
+  LOG(INFO) << "Test " << ++testNumber << ":[IsContainer] No Errors Expected";
   test = utility::IsContainer< std::string >::value;
   REQUIRE( test );
+  LOG(INFO) << "Test " << ++testNumber << ":[IsContainer] No Errors Expected";
   test = utility::IsContainer< std::string::iterator >::value;
   REQUIRE( not test );
+  LOG(INFO) << "Test " << ++testNumber << ":[IsContainer] No Errors Expected";
   test = utility::IsContainer< int >::value;
   REQUIRE( not test );
 }
