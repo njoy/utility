5a84f6aa59076a14aefb074dfe40a465c8de78d0
Austin McCartney <amccartney@lanl.gov>
2016-10-25 13:38:20 -0600

    build scripts

--- .cygwin/build.sh
+++ .cygwin/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+alias python='python3'
 mkdir build
 cd ./build
 cmake -D build_type=$build_type \
       -D static_libraries=$static_libraries \
       -D appended_flags="$appended_flags" \
-      -D no_link_time_optimization=TRUE ..
+      -D no_link_time_optimization=TRUE \
+      -G "Unix Makefiles" ..
 make -j2
 export COMPILATION_FAILURE=$?
 ctest --output-on-failure -j2
