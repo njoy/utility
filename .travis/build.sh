1c8ea18a44cbd3a120d2654b970335df0c8c936e
Austin McCartney <austin.p.mccartney@gmail.com>
2016-10-26 03:06:27 +0000

    array expansion

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,12 +5,12 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 90 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-3.8
     export appended_flags=$appended_flags"-stdlib=libstdc++"
     export PATH=/usr/bin:$PATH
-    export CUSTOM='-D no_link_time_optimization=TRUE'
+    export CUSTOM=('-D no_link_time_optimization=TRUE')
     sudo update-alternatives --config clang
   else
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 --slave /usr/bin/g++ g++ /usr/bin/g++-6 --slave /usr/bin/gcov gcov /usr/bin/gcov-6
     sudo update-alternatives --config gcc
-    export CUSTOM="-D CMAKE_AR=/usr/bin/gcc-ar-6 -D CMAKE_RANLIB=/usr/bin/gcc-ranlib-6"
+    export CUSTOM=("-D CMAKE_AR=/usr/bin/gcc-ar-6" "-D CMAKE_RANLIB=/usr/bin/gcc-ranlib-6")
   fi;
 fi
 
@@ -19,7 +19,7 @@ cd build
 cmake -D build_type=$build_type \
       -D static_libraries=$static_libraries \
       -D appended_flags="$appended_flags" \
-      $CUSTOM ..
+      ${CUSTOM[@]} ..
 make -j2
 export COMPILATION_FAILURE=$?
 ctest --output-on-failure -j2
