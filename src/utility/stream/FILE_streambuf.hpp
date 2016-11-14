d09d042f73e79c9e384698df8f8d4d27ae2be1fd
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2016-11-14 15:13:39 -0700

    Fixing error in capitilzation. Again.

--- src/utility/stream/FILE_streambuf.hpp
+++ src/utility/stream/FILE_streambuf.hpp
@@ -1,17 +1,17 @@
 template< class Char_t, class Traits = std::char_traits< Char_t > >
 class FILE_streambuf : public std::basic_streambuf< Char_t, Traits > {
 public:
-#include "utility/stream/File_streambuf/src/ctor.hpp"
-#include "utility/stream/File_streambuf/src/dtor.hpp"
+#include "utility/stream/FILE_streambuf/src/ctor.hpp"
+#include "utility/stream/FILE_streambuf/src/dtor.hpp"
 
   FILE* filePtr;
   const std::size_t putBack;
   std::vector< char > buffer;
 
 private:
-#include "utility/stream/File_streambuf/src/flush.hpp"
-#include "utility/stream/File_streambuf/src/overflow.hpp"
-#include "utility/stream/File_streambuf/src/underflow.hpp"
+#include "utility/stream/FILE_streambuf/src/flush.hpp"
+#include "utility/stream/FILE_streambuf/src/overflow.hpp"
+#include "utility/stream/FILE_streambuf/src/underflow.hpp"
     
   typename Traits::int_type sync(){ return this->flush() ? 0 : -1; }
 };
